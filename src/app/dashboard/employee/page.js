00a0fc81448c003e6f7163e634ca1429ccfaf822
Dev-megh <chevlimegh@gmail.com>
2026-01-03 14:35:38 +0530

    fix employee side

--- src/app/dashboard/employee/page.js
+++ src/app/dashboard/employee/page.js
@@ -2,7 +2,7 @@ import EmployeeSide from "@/components/EmployeeSide";
 export default function EmployeeDashboardPage() {
   return (
     <main className="min-h-screen bg-[#0B0F1A] text-gray-200 flex">
-      <EmployeeSide/>
+      <EmployeeSide screen="dashboard"/>
       <div className="flex-1">
         <section className="p-6 space-y-8">
           
--- src/components/EmployeeSide.js
+++ src/components/EmployeeSide.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import NavItem from './NavItem'
 
-const EmployeeSide = () => {
+const EmployeeSide = ({screen}) => {
   return (
     <aside className="w-64 bg-white/5 border-r border-white/10 hidden md:block">
         <div className="p-6 text-2xl font-bold text-indigo-400">DayFlow</div>
 
         <nav className="px-4 space-y-2">
-          <NavItem label="Dashboard" active  href="/dashboard/employee"/>
-          <NavItem label="My Attendance" href="/attendance/employee" />
-          <NavItem label="My Leaves" href="/leave/apply" />
-          <NavItem label="Profile" href=""/>
+          <NavItem label="Dashboard" href="/dashboard/employee" active={screen === "dashboard"} />
+          <NavItem label="My Attendance" href="/attendance/employee" active={screen === "attendance"} />
+          <NavItem label="My Leaves" href="/leave/apply" active={screen === "leave"} />
+          <NavItem label="Profile" href="" active={screen === "profile"} />
         </nav>
       </aside>
   )
