ce6f8589b0611c45a36deef277f3c6c7a8e34430
Dev-megh <chevlimegh@gmail.com>
2026-01-03 11:03:19 +0530

    added blank pages to files

---
+++ src/app/attendance/page.js
@@ -0,0 +1,7 @@
+export default function AttendancePage() {
+    return (
+        <main className="min-h-screen bg-[#0B0F1A] text-gray-200 flex items-center justify-center">
+        <h1 className="text-4xl font-bold">Attendance Page</h1>
+        </main>
+    );
+}
