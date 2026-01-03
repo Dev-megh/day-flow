e374fd962ff9ab7f1b1223be10f6731d4c7e4834
Dev-megh <chevlimegh@gmail.com>
2026-01-03 14:23:41 +0530

    update admin sidebar

--- src/components/AdminSide.js
+++ src/components/AdminSide.js
@@ -1,20 +1,20 @@
 import NavItem from "./NavItem"
 
-function AdminSide() {
+function AdminSide({screen}) {
   return (
     <aside className="w-64 bg-white/5 border-r border-white/10 hidden md:block">
         <div className="p-6 text-2xl font-bold text-indigo-400">DayFlow</div>
 
         <nav className="px-4 space-y-2">
-            <NavItem label="Dashboard" href="/dashboard/admin" active />
+            <NavItem label="Dashboard" href="/dashboard/admin" active={screen === "dashboard"} />
 
-            <NavItem label="Employees" href="/dashboard/admin" />
+            <NavItem label="Employees" href="/dashboard/admin" active={screen === "employees"} />
 
-            <NavItem label="Attendance" href="/attendance/admin" />
+            <NavItem label="Attendance" href="/attendance/admin" active={screen === "attendance"} />
 
-            <NavItem label="Leaves" href="/leave/admin/list" />
+            <NavItem label="Leaves" href="/leave/admin/list" active={screen === "leaves"} />
 
-            <NavItem label="Payroll" href="/payroll/admin" />
+            <NavItem label="Payroll" href="/payroll/admin" active={screen === "payroll"} />
         </nav>
     </aside>
   )
